f797dbc80d0318c12197f7b2c30310a25423bd97
ImanElomda <emanelomda9@gmail.com>
2023-04-29 02:43:41 +0200

    22/8

--- modules/KolbStyle/KolbStyle.route.js
+++ modules/KolbStyle/KolbStyle.route.js
@@ -3,4 +3,5 @@ import * as kc from './controller/KolbStyle.controller.js'
 const router = Router()
 
 router.post("/addkolbStyle",kc.addKolbStyle)
+router.get("/getLesssonNumber",kc.getLesssonNumber)
 export default router
--- modules/KolbStyle/controller/KolbStyle.controller.js
+++ modules/KolbStyle/controller/KolbStyle.controller.js
@@ -4,7 +4,7 @@ import { KolbStyleModel } from "../../../DB/model/KolbStyle.model.js";
 export const addKolbStyle = async (req, res) => {
     try {
         const { character, learning_styles } = req.body;
-        const KolbStyle = new KolbStyleModel({ character, learning_styles})
+        const KolbStyle = new KolbStyleModel({ character, learning_styles })
         const savedkolbStyle = await KolbStyle.save()
         res.json({ message: "Done", savedkolbStyle })
     } catch (error) {
@@ -12,14 +12,33 @@ export const addKolbStyle = async (req, res) => {
         console.log(error);
     }
 };
-export const getLearninStyle = async (req, res) => {
-    try {
-        const { character} = req.body;
-        const learning_styles = await KolbStyleModel.find({learning_styles})
-        const savedkolbStyle = await KolbStyle.save()
-        res.json({ message: "Done", savedkolbStyle })
-    } catch (error) {
-        res.json({ message: "error", error })
-        console.log(error);
+// export const getLearninStyle = async (req, res) => {
+//     try {
+//         const { character} = req.body;
+//         const learning_styles = await KolbStyleModel.find({character}).select('learning_styles -_id')
+//         if (learning_styles) {
+//                console.log(learning_styles);
+//         } else {
+//             console.log(errorrrr);
+//         }
+
+//     } catch (error) {
+//         res.json({ message: "error", error })
+//         console.log(error);
+//     }
+// };
+export const getLesssonNumber = async (req, res) => {
+    const { character } = req.body
+    const style = await KolbStyleModel.findOne({ character }).select('learning_styles')
+    if (!style) {
+        res.status(500).json({ message: "error" })
+    } else {
+        res.json({ message: "Done", learning_styles: style.learning_styles })
+        if (style.learning_styles == "direct") {
+               lessonNumber=[currentLesson,currentLesson]
+        } else {
+
+        }
     }
-};
+
+}
